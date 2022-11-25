21f5d115a793dbdefb1bc59134d79e4822c962b4
Ysquid <ahk648@gmail.com>
2022-11-25 09:35:46 -0600

    post new minion route

--- server/api.js
+++ server/api.js
@@ -9,5 +9,14 @@ apiRouter.get('/minions/', (req, res, next) => {
     res.send(minions)
 })
 
+//post new minion to DB
+//name == string, weakness ==== string, title===string, salary ===number
+//addToDatabase does the validation and incremeting of id
+//we just need to provide the model type and instance values (i.e post body)
+apiRouter.post('/minions/', (req, res, next) => {
+    const newMinion = req.query
+    res.send(db.addToDatabase('minions', newMinion))
+})
+
 
 module.exports = apiRouter;
