c0aeac496ebf62a7f06baf6dfba9b13c01a72027
Ysquid <ahk648@gmail.com>
2022-11-25 10:01:07 -0600

    finished minions routes - GET by ID, PUT by ID, DELETE by ID

--- server/api.js
+++ server/api.js
@@ -2,20 +2,61 @@ const express = require('express');
 const apiRouter = express.Router();
 const db = require('./db.js')
 
-//get array of all minions
+//GET array of all minions
 apiRouter.get('/minions/', (req, res, next) => {
     const minions = db.getAllFromDatabase('minions')
     console.log(minions)
     res.send(minions)
 })
 
-//post new minion to DB
-//name == string, weakness ==== string, title===string, salary ===number
+//POST new minion to DB
+//name == string, weaknesses ==== string, title===string, salary ===number
 //addToDatabase does the validation and incremeting of id
 //we just need to provide the model type and instance values (i.e post body)
 apiRouter.post('/minions/', (req, res, next) => {
     const newMinion = req.query
     res.send(db.addToDatabase('minions', newMinion))
+})
+
+//GET single minion by Id
+apiRouter.get('/minions/:minionId', (req, res, next) => {
+    const id = req.params.minionId
+    response = db.getFromDatabaseById('minions', id)
+    if (response) {
+        res.send(response)
+    } else {
+        res.status(404).send('No minion found for that id')
+    }
+})
+
+//PUT minion by Id
+apiRouter.put('/minions/:minionId', (req, res, next) => {
+    const id = req.params.minionId
+    const updatedInstance = {
+        id: id,
+        name: req.query.name,
+        title: req.query.title,
+        weaknesses: req.query.weaknesses,
+        salary: req.query.salary,
+    }
+    response = db.updateInstanceInDatabase('minions', updatedInstance)
+
+    if(response) {
+        res.send(response)
+    } else {
+        res.status(400).send('bad update request')
+    }
+})
+
+//DELETE minion by Id
+apiRouter.delete('/minions/:minionId', (req, res, next) => {
+    const id = req.params.minionId
+    const response = db.deleteFromDatabasebyId('minions', id)
+    if (response) {
+        res.send(response)
+    } else {
+        res.status(404).send('minion with that ID not found')
+    }
 })
 
 
