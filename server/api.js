35787d7be1be9b014b5c5beea262fcfc972b0448
Ysquid <ahk648@gmail.com>
2022-11-25 11:10:13 -0600

    finished all ideas routes

--- server/api.js
+++ server/api.js
@@ -13,18 +13,26 @@ const db = require("./db.js");
 
 //GET array of all minions
 apiRouter.get("/minions/", (req, res, next) => {
-  const minions = db.getAllFromDatabase("minions");
-  console.log(minions);
-  res.send(minions);
+  const response = db.getAllFromDatabase("minions");
+  if (response) {
+    res.send(response);
+  } else {
+    res.status(404).send("cannot GET minions");
+  }
 });
 
 //POST new minion to DB
 apiRouter.post("/minions/", (req, res, next) => {
   const newMinion = req.query;
-  res.send(db.addToDatabase("minions", newMinion));
+  const response = db.addToDatabase("minions", newMinion);
+  if (response) {
+    res.send(response);
+  } else {
+    res.status(400).send("could not POST new minion");
+  }
 });
 
-//GET single minion by Id
+//GET single minion by ID
 apiRouter.get("/minions/:minionId", (req, res, next) => {
   const id = req.params.minionId;
   response = db.getFromDatabaseById("minions", id);
@@ -35,7 +43,7 @@ apiRouter.get("/minions/:minionId", (req, res, next) => {
   }
 });
 
-//PUT minion by Id
+//PUT minion by ID
 apiRouter.put("/minions/:minionId", (req, res, next) => {
   const id = req.params.minionId;
   const updatedInstance = {
@@ -54,7 +62,7 @@ apiRouter.put("/minions/:minionId", (req, res, next) => {
   }
 });
 
-//DELETE minion by Id
+//DELETE minion by ID
 apiRouter.delete("/minions/:minionId", (req, res, next) => {
   const id = req.params.minionId;
   const response = db.deleteFromDatabasebyId("minions", id);
@@ -65,4 +73,76 @@ apiRouter.delete("/minions/:minionId", (req, res, next) => {
   }
 });
 
+/**
+ Idea Schema:
+  Idea:
+    id: string
+    name: string
+    description: string
+    numWeeks: number
+    weeklyRevenue: number
+**/
+
+//GET all ideas
+apiRouter.get("/ideas/", (req, res, next) => {
+  const response = db.getAllFromDatabase("ideas");
+  if (response) {
+    res.send(response);
+  } else {
+    res.status(404).send("cannot GET ideas");
+  }
+});
+
+//POST new idea
+apiRouter.post("/ideas/", (req, res, next) => {
+  const newIdea = req.query;
+  const response = db.addToDatabase("ideas", newIdea);
+  if (response) {
+    res.send(response);
+  } else {
+    res.status(400).send("cannot POST new idea");
+  }
+});
+
+//GET idea by ID
+apiRouter.get("/ideas/:ideaId", (req, res, next) => {
+  const id = req.params.ideaId;
+  const response = db.getFromDatabaseById("ideas", id);
+  if (response) {
+    res.send(response);
+  } else {
+    res.status(404).send("cannot GET idea with that ID");
+  }
+});
+
+//PUT idea by ID
+apiRouter.put("/ideas/:ideaId", (req, res, next) => {
+  const id = req.params.ideaId;
+  const { name, description, numWeeks, weeklyRevenue } = req.query;
+  const updatedInstance = {
+    id: id,
+    name: name,
+    description: description,
+    numWeeks: numWeeks,
+    weeklyRevenue: weeklyRevenue,
+  };
+
+  response = db.updateInstanceInDatabase("ideas", updatedInstance);
+  if (response) {
+    res.send(response);
+  } else {
+    res.status(400).send("bad update request");
+  }
+});
+
+//DELETE idea by ID
+apiRouter.delete("/ideas/:ideaId", (req, res, next) => {
+    const id = req.params.ideaId;
+    const response = db.deleteFromDatabasebyId('ideas', id)
+    if (response) {
+        res.send(response)
+    } else {
+        res.status(404).send('idea not found')
+    }
+})
 module.exports = apiRouter;
