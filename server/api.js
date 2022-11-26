6e4cd8c24d79b93e81ddba2069d464ca347c8f53
Ysquid <ahk648@gmail.com>
2022-11-26 11:46:28 -0600

    meetings route in seperate module. mounted.

--- server/api.js
+++ server/api.js
@@ -1,36 +1,11 @@
 const express = require("express");
 const apiRouter = express.Router();
-const db = require("./db.js");
 const ideasRouter = require("./ideas.js");
+const meetingsRouter = require("./meetings.js");
 const minionsRouter = require("./minions.js");
 
 apiRouter.use('/minions/', minionsRouter)
 apiRouter.use('/ideas/', ideasRouter)
-
-/*
-MEETING SCHEMA:
-Meeting
-  time: string
-  date: JS Date object
-  day: string
-  note: string
-*/
-
-//GET Meetings
-apiRouter.get("/meetings/", (req, res, next) => {
-  res.send(db.getAllFromDatabase("meetings"));
-});
-
-//POST Meeting
-apiRouter.post("/meetings/", (req, res, next) => {
-  const newMeeting = db.createMeeting()
-  res.status(201).send(db.addToDatabase("meetings", newMeeting))
-
-});
-
-//DELETE All Meetings
-apiRouter.delete("/meetings/", (req, res, body) => {
-  res.status(204).send(db.deleteAllFromDatabase('meetings'))
-})
+apiRouter.use('/meetings', meetingsRouter)
 
 module.exports = apiRouter;
